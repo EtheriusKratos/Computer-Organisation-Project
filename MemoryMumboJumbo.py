49ccb81311ce43a090300eb9ef26aa65ce97bfdd
nav-rtd <108548594+nav-rtd@users.noreply.github.com>
2022-08-06 21:30:52 +0530

    Update MemoryMumboJumbo.py

--- MemoryMumboJumbo.py
+++ MemoryMumboJumbo.py
@@ -14,3 +14,19 @@ def type_1():
     elif change == 4:
         nP +=  math.log2(CPU_bits / 8)
     print("Address pins saved of required are", P-nP)
+    
+    def type_2():    
+    CPU_bits = int(input("Enter no of bits in CPU: "))
+    pins_count = int(input("Enter the no. of address pins: "))
+    change = int(input("Enter the new addressable memory option: "))
+    while (change > 4 or change < 1):
+        print("Wrong Type.. :(")
+        change = int(input("Enter the type of memory: "))
+    answer = (2 ** pins_count)
+    if change == 1:
+        answer = (2 * -3)
+    elif change == 2:
+        answer = (2 * -1)
+    elif change == 4:
+        answer *=  (CPU_bits / 8)
+    print("Max memory in Bytes:", (answer / (2 ** 30)), "GB")
